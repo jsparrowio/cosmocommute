a7ba75f7bfb5186026125dc5262b59e2e7ba5464
jsparrowio <jsparrowio@outlook.com>
2024-12-10 19:16:55 -0800

    added base file and directory structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Outlet } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import './App.css';
+//import Header from './components/Header';
+///import Footer from './components/Footer';
 
+// function to render entire React single page application
 function App() {
-  const [count, setCount] = useState(0)
-
+  const location = useLocation();
+  // establishes about me as home page
+  const [currentPage, setPage] = useState("About Me")
+  useEffect(() => {
+    if (location.pathname === '/') {
+      setPage("Home");
+    } else if (location.pathname === '/login') {
+      setPage("Login");
+    } else if (location.pathname === '/dashboard') {
+      setPage("Dashboard");
+    } else if (location.pathname === '/weather') {
+      setPage("Weather");
+    } else if (location.pathname === '/traffic') {
+      setPage("Traffic");
+    }
+    document.title = `${currentPage} | CosmoCommute`;
+  });
+  // returns the entire single page site, outlet based on what the user chooses
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     {/* <Header /> */}
+      <Outlet />
+     {/* <Footer /> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
